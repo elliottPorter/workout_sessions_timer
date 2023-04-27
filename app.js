3b149931e35ef523109816a6d5fa0d77d0fb068d
Elliott Porter <elliott@weblake.co.uk>
2023-04-27 17:29:41 +0100

    array without objects

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const main_timer = document.getElementById('main_timer');
 const beep = document.getElementById('beep');
+const userSubmit = document.getElementById('userSubmit');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const timers = [];
 
 const main_output = () => {
 	main_timer.textContent = count;
 };
 
-const countdown = (num) => {
-	let count = num;
+console.log('The array is ' + timers);
+
+const start = () => {
+	for (let i = 0; i < timers.length; i++) {
+		countdown(i);
+	}
+};
+
+const countdown = (i) => {
+	let count = Number(timers[i]);
+	console.log('The current count is ' + count);
 	main_timer.textContent = count;
 	let countdown = setInterval(() => {
 		count--;
@@ -16,4 +29,16 @@ const countdown = (num) => {
 		}
 		main_timer.textContent = count;
 	}, 1000);
+	return;
 };
+
+const pushTimerOptions = (e) => {
+	e.preventDefault();
+	let totalSeconds = Number(minutes.value) * 60 + Number(seconds.value);
+	timers.push(totalSeconds);
+	console.log(timers);
+	return totalSeconds;
+};
+
+// create the event listeners
+userSubmit.addEventListener('click', pushTimerOptions, false);
